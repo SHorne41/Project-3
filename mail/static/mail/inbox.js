30197f44d445a82d0a83b5b64e40e2d92c37bd33
SHorne41 <stephane.horne@gmail.com>
2020-08-20 11:31:16 -0400

    Emails are retrieved from the API and displayed in the console; not yet displayed in the DOM

--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -27,6 +27,14 @@ function send_mail() {
     });
 }
 
+function load_mail() {
+    fetch('/emails/inbox')
+    .then(response => response.json())
+    .then(emails => {
+        console.log(emails);
+    });
+}
+
 function compose_email() {
 
   // Show compose view and hide other views
@@ -47,4 +55,8 @@ function load_mailbox(mailbox) {
 
   // Show the mailbox name
   document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
+
+  if (mailbox === 'inbox') {
+      load_mail();
+  }
 }
