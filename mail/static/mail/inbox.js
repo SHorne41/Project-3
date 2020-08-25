38f535146caecfda9f4956dce9f6195ee6c9c5cc
SHorne41 <stephane.horne@gmail.com>
2020-08-25 10:42:11 -0400

    Closed '#view-email' div if mailbox view was opened

--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -166,6 +166,7 @@ function compose_email() {
 
   // Show compose view and hide other views
   document.querySelector('#emails-view').style.display = 'none';
+  document.querySelector('#view-email').style.display = 'none';
   document.querySelector('#compose-view').style.display = 'block';
 
   // Clear out composition fields
